1bf481e474183133b93fcb6a0f52bc879d5f0b1a
KokinSSJ <31051991@interia.pl>
2023-05-30 16:06:58 +0200

    adding script for influxdb backup

---
+++ tools/backup_influxdb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "InfluxDB backup started"
+
+TOKEN=`cat /ssd/home-assistant/secrets.yaml | grep INFLUXDB_INIT_ADMIN_TOKEN | sed 's/INFLUXDB_INIT_ADMIN_TOKEN:*//g'| xargs`
+
+#echo $TOKEN
+
+BACKUP_TIME=$(date '+%Y-%m-%d_%H-%M')
+echo $BACKUP_TIME
+
+docker exec -it influxdb influx backup /backups/backup_$BACKUP_TIME -t $TOKEN
+
+tar -czvf backup_$BACKUP_TIME.tar.gz /ssd/backups/influxdb/backup_$BACKUP_TIME
+
+rm -rf /ssd/backups/influxdb/backup_$BACKUP_TIME/
+
+echo "InfluxDB backup done"
