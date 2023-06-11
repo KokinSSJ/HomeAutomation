51793b439b62b69df10610e4e8758fef93c23a5e
KokinSSJ <31051991@interia.pl>
2023-06-11 07:08:04 +0200

    adding date to backups scripts to see when executed

--- tools/backup_influxdb.sh
+++ tools/backup_influxdb.sh
@@ -2,6 +2,8 @@
 
 echo "InfluxDB backup started"
 
+date
+
 TOKEN=`cat /ssd/home-assistant/secrets.yaml | grep INFLUXDB_INIT_ADMIN_TOKEN | sed 's/INFLUXDB_INIT_ADMIN_TOKEN:*//g'| xargs`
 
 #echo $TOKEN
@@ -15,5 +17,7 @@ docker exec influxdb influx backup /backups/backup_$BACKUP_TIME -t $TOKEN
 sudo tar -czvf /ssd/backups/influxdb/backup_$BACKUP_TIME.tar.gz /ssd/backups/influxdb/backup_$BACKUP_TIME
 
 sudo rm -rf /ssd/backups/influxdb/backup_$BACKUP_TIME/
+
+date
 
 echo "InfluxDB backup done"
--- tools/backup_mariadb.sh
+++ tools/backup_mariadb.sh
@@ -3,6 +3,8 @@
 
 echo "MariaDB backup started"
 
+date
+
 TOKEN=`cat /ssd/home-assistant/secrets.yaml | grep MARIADB_URL | sed 's/.*://' | sed 's/@.*//' | xargs`
 
 #echo $TOKEN
@@ -11,6 +13,8 @@ BACKUP_TIME=$(date '+%Y-%m-%d_%H-%M')
 echo $BACKUP_TIME
 
 docker exec mariadb bash -c "mysqldump -u homeassistant -p$TOKEN --all-databases | gzip > /backups/database_$BACKUP_TIME.sql.gz"
+
+date
 
 echo "MariaDB backup done"
 #set +x
